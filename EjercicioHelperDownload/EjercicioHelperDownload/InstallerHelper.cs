a4f9afccb6b3c0a11fcb643e1882a47367437658
cjcarrer <cjcarrer@bancolombia.com.co>
2021-10-26 22:34:40 -0500

    FirstCommit

---
+++ EjercicioHelperDownload/EjercicioHelperDownload/InstallerHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+
+
+namespace EjercicioHelperDownload
+{
+    public class InstallerHelper
+    {
+        private string _setupDestinationFile;
+
+        public bool DownloadInstaller(string customerName, string installerName)
+        {
+            var client = new WebClient();
+            try
+            {
+                client.DownloadFile(
+                    string.Format("http://example.com/{0}/{1}",
+                        customerName,
+                        installerName),
+                    _setupDestinationFile);
+
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+    }
+}
